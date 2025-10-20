11f182bfff07811a644177fe5952c6f649af6160
Benicio <sabarrosbenicio@gmail.com>
2025-10-20 16:46:24 +0000

    Create the logic for playing a round

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ window.onload = function(){
                        "", "", "",
                        "", "", ""];
 
-        const changeValue = (row, column, player) => {
-            board[row][column] = player.symbol;
+        const printBoard = () => {
+            console.log(board);
+        }
+
+        return {
+            board,
+            printBoard
         }
     }
 
     function GameController(playerOneName = "Player 1", playerTwoName = "Player 2"){
-        const players = [
+        const board = Gameboard(); // create a new gameboard object
+
+        const players = [ 
             {
                 name: playerOneName,
                 symbol: "X",
@@ -20,8 +27,15 @@ window.onload = function(){
                 name: playerTwoName,
                 symbol: "O",
                 wins: 0
-            }
-        ]
+            }];
+
+        // logic for turn switching
+        let activePlayer = players[0];
+
+        const switchPlayerTurn = () => {
+            activePlayer = activePlayer === players[0] ? players[1] : players[0];
+        };
+
         const winningCombos = [[0, 1, 2], // rows
                                [3, 4, 5], 
                                [6, 7, 8],
@@ -33,20 +47,37 @@ window.onload = function(){
         
         let roundCounter = 0;
 
-        const checkForWins = function(){
+        const checkForWins = () => {
             for(const [a, b, c] of winningCombos){
                 if(!board[a] && board[a] === board[b] && board[b] === board[c]){
                     for(player of players){
                         if(player.symbol === board[a]){
-                            player.wins++;
-                            break;
+                            console.log(`${player.name} wins!`);
+                            console.log("Starting new Round");
+                            // GameController(players[0].name, players[1].name) ?
+                            return;
                         }
                     }
                 }
             }
         }
 
-        
+        const printNewRound = () => {
+            board.printBoard();
+            console.log(`${activePlayer.name}'s turn`)
+        }
+
+        // change value of selected cell with current player's token if at least 5 rounds have passed check for win condition, start new round
+        const playRound = (value) => {
+            board.board[value] = activePlayer.symbol;
+            roundCounter++;
+            if(roundCounter > 4) checkForWins();
+            switchPlayerTurn();
+            printNewRound();
+        }
+        return {
+            playRound
+        }
     }
-    
+    const game = GameController();
 }
