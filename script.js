22bb08d3fa26d03b27211e8177b195164f25c672
Benicio <sabarrosbenicio@gmail.com>
2025-10-21 01:19:19 +0000

    Finish DOM logic

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 
-    function Gameboard(){
+    function GameController(playerOneName = "Player 1", playerTwoName = "Player 2"){
         const board = ["", "", "",
                        "", "", "",
-                       "", "", ""];
+                       "", "", ""]; // create a new gameboard object
 
-        const printBoard = () => {
-            console.log(board);
-        }
-
-        return {
-            board,
-            printBoard
-        }
-    }
-
-    function GameController(playerOneName = "Player 1", playerTwoName = "Player 2"){
-        const board = Gameboard(); // create a new gameboard object
-
-        const cells = document.querySelectorAll();
+        const display = document.querySelector("#display");
+        const score = document.querySelector("#score");
+        const currentPlayerDisplay = document.querySelector("#current-player");
+        const wins1 = document.querySelector("#wins-1");
+        const wins2= document.querySelector("#wins-2");
+        const cells = document.querySelectorAll(".cell");
         for(const cell of cells){
-            
+            cell.addEventListener("click", () => {
+                const cellIndex = parseInt(cell.id);
+                playRound(cellIndex);
+            })
         }
 
         const players = [ 
@@ -36,6 +31,7 @@
 
         // logic for turn switching
         let activePlayer = players[0];
+        let isGameActive = true;
 
         const switchPlayerTurn = () => {
             activePlayer = activePlayer === players[0] ? players[1] : players[0];
@@ -56,52 +52,67 @@
 
         const checkForWins = () => {
             for(const [a, b, c] of winningCombos){
-                if(board.board[a] && board.board[a] === board.board[b] && board.board[b] === board.board[c]){
+                if(board[a] && board[a] === board[b] && board[b] === board[c]){
                     for(const player of players){
-                        if(player.symbol === board.board[a]){
-                            console.log(`${player.name} wins!`);
+                        if(player.symbol === board[a]){
+                            display.textContent = `${player.name} wins!`;
                             player.wins++;
-                            console.log(`Current score --> ${playerOneName}: ${players[0].wins}, ${playerTwoName}: ${players[1].wins}`);
-                            console.log("Starting new match");
-                            resetMatch();
+                            wins1.textContent = `Wins: ${players[0].wins}`;
+                            wins2.textContent = `Wins: ${players[1].wins}`;
+                            score.textContent = `Current score --> ${playerOneName}: ${players[0].wins}, ${playerTwoName}: ${players[1].wins}`;
+                            cells[a].classList.add("winner");
+                            cells[b].classList.add("winner");
+                            cells[c].classList.add("winner");
+                            isGameActive = false;
+                                setTimeout(() => {
+                                resetMatch();
+                                isGameActive = true;
+                                cells[a].classList.remove("winner");
+                                cells[b].classList.remove("winner");
+                                cells[c].classList.remove("winner");
+                                }, 1000);
                             return;
                         }
                     }
                 }
             }
             if (roundCounter === 9){
-                    console.log("It's a tie!");
-                    console.log(`Current score --> ${playerOneName}: ${players[0].wins}, ${playerTwoName}: ${players[1].wins}`);
-                    console.log("Starting new match");
-                    switchPlayerTurn();
-                    resetMatch();
+                    display.textContent = "It's a tie!";
+                    score.textContent = `Current score --> ${playerOneName}: ${players[0].wins}, ${playerTwoName}: ${players[1].wins}`;
+                    setTimeout(resetMatch, 1000);
                     return;
                 }
         }
 
         const printNewRound = () => {
-            board.printBoard();
-            console.log(`${activePlayer.name}'s turn`)
+            let i = 0;
+            for(const cell of cells){
+                cell.textContent = board[i];
+                i++;
+            }
+            currentPlayerDisplay.textContent = `${activePlayer.name}'s turn`;
         }
 
         // change value of selected cell with current player's token if at least 5 rounds have passed check for win condition, start new round
         const playRound = (value) => {
-            if(!board.board[value]) {
-                board.board[value] = activePlayer.symbol;
+            if(!isGameActive) return;
+            if(!board[value]) {
+                board[value] = activePlayer.symbol;
                 roundCounter++;
-                if(roundCounter > 4) checkForWins();
+                if(roundCounter > 4){
+                    checkForWins();
+                    if(roundCounter === 0) return;
+                }
                 switchPlayerTurn();
                 printNewRound();
-            } else {
-                console.log("Cell already taken");
-            }
-        }
+        }}
 
         const resetMatch = () => {
-            for(let i = 0; i < board.board.length; i++){
-                board.board[i] = "";
+            for(let i = 0; i < board.length; i++){
+                board[i] = "";
             }
             roundCounter = 0;
+            display.textContent = "";
             printNewRound();
         }
 
